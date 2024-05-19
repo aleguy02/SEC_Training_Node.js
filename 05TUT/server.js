30388bb9bb82ade3ab2dd6fe541df782e3228cad
Ale V <alejandro.the.villate@gmail.com>
2024-05-18 20:08:54 -0400

    Finished Lesson 9

--- 05TUT/server.js
+++ 05TUT/server.js
@@ -2,36 +2,27 @@ const express = require("express")
 const app = express()
 const path = require('path')
 const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const {logger} = require('./middleware/logEvents')
 const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500
 
+// custome middleware logger
 app.use(logger)
 
-const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    optionsSuccessStatus: 200
-}
+// Cross Origin Resource Sharing
 app.use(cors(corsOptions))
 
+// built in middleware for form data
 app.use(express.urlencoded({ extended: false}))
 
 app.use(express.json())
 
 // serve static files
 app.use(express.static(path.join(__dirname, '/public')))
-app.use('/subdir', express.static(path.join(__dirname, '/public')))
 
 // routes
 app.use('/', require('./routes/root'))
-app.use('/subdir', require('./routes/subdir'))
 app.use('/employees', require('./routes/api/employees'))
 
 // app.use('/')
